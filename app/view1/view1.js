5f1b92d3c8d6573a416d4b3faacd9ab8c0ed24bd
imorelnunez <ivanmorel94@gmail.com>
2017-12-15 19:16:57 -0400

    push

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -34,4 +34,14 @@ angular.module('myApp.view1', ['ngRoute'])
     $scope.rand = $scope.randaux;
     $scope.food = $scope.types[$scope.rand];
   }
+    var _name = 'Brian';
+    $scope.user = {
+        name: function(newName) {
+            // Note that newName can be undefined for two reasons:
+            // 1. Because it is called as a getter and thus called with no arguments
+            // 2. Because the property should actually be set to undefined. This happens e.g. if the
+            //    input is invalid
+            return arguments.length ? (_name = newName) : _name;
+        }
+    };
 }]);
---
+++ app/view3/view3.js
@@ -0,0 +1,22 @@
+'use strict';
+angular.module('myApp.view3', ['ngRoute'])
+    .config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/view3', {
+            templateUrl: 'view3/view3.html',
+            controller: 'View3Ctrl'
+        });
+    }])
+    .controller('View3Ctrl', ['$scope', function($scope) {
+        $scope.friends = [
+            {name:'John', age:25, gender:'boy'},
+            {name:'Jessie', age:30, gender:'girl'},
+            {name:'Johanna', age:28, gender:'girl'},
+            {name:'Joy', age:15, gender:'girl'},
+            {name:'Mary', age:28, gender:'girl'},
+            {name:'Peter', age:95, gender:'boy'},
+            {name:'Sebastian', age:50, gender:'boy'},
+            {name:'Erika', age:27, gender:'girl'},
+            {name:'Patrick', age:40, gender:'boy'},
+            {name:'Samantha', age:60, gender:'girl'}
+            ]
+    }]);
