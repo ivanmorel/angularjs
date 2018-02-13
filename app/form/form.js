b3ba01b6d4ad409866a8a1e2134b354ccab6fb6c
imorelnunez <ivanmorel94@gmail.com>
2018-02-12 20:00:42 -0400

    push

--- app/form/form.js
+++ app/form/form.js
@@ -7,4 +7,49 @@ angular.module('myApp.form', ['ngRoute', 'ngAnimate', 'myApp'])
         });
     }])
     .controller('formCtrl', ['$scope', '$timeout', 'highscore', '$http', '$filter', function($scope, $timeout, highscore, $http, $filter) {
+        $http.get("http://127.0.0.1:3000/users/json").then(function(response){
+            $scope.users = response.data;
+        });
+        $scope.found = false;
+        $scope.founduser = false;
+        var cont = true;
+        $scope.testx = "";
+        $scope.isIngameAvailable = function(user){
+            $scope.register= false;
+            $scope.users.forEach(function(v,k){
+                if(cont){
+                    if(v.ingame==user){
+                        $scope.found = true;
+                        cont = false;
+                    }else{
+                        $scope.found = false;
+                    }
+                }
+            });
+            cont = true;
+        };
+
+        $scope.isUserAvailable = function(user){
+            $scope.register= false;
+            $scope.users.forEach(function(v,k){
+                if(cont){
+                    if(v.username==user){
+                        $scope.founduser = true;
+                        cont = false;
+                    }else{
+                        $scope.founduser = false;
+                    }
+                }
+            });
+            cont = true;
+        };
+
+        $scope.clear = function(){
+            $http.get("http://127.0.0.1:3000/users/json").then(function(response){
+                $scope.users = response.data;
+                $scope.user = {'ingame': "", 'username': "", 'password': "", 'email': ""};
+                $scope.rpassword = "";
+                $scope.register= true;
+            });
+        }
     }]);
